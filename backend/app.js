6cde942917ebc1acdb0f73e320b677b8923443fb
Awet Thon <awetthon8@gmail.com>
2026-07-07 14:15:19 +0200

    minor update

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 import express from "express";
 
 const app = express();
+
+// log incoming request and timestamps
+app.use((req, res, next) => {
+  const now = new Date();
+
+  console.log(`${now}: Incoming ${req.method} request from ${req?.ip}`);
+  next();
+});
+
 const PORT = 8000;
 app.get("/", async (req, res) => {
   res.send("Backend: up and running mate.");
