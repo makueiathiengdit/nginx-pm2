c67981314c78fc01ae2caaa5fe0b2e4d1a121bbb
Awet Thon <awetthon8@gmail.com>
2026-07-08 15:56:35 +0200

    receive data from frontend

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.json());
 
+let last_saved_idx = 0;
+
 // log incoming request and timestamps
 app.use((req, res, next) => {
   const now = new Date();
@@ -16,6 +18,14 @@ const PORT = 8000;
 app.get("/", async (req, res) => {
   res.setHeader("X-Powered-By", "Awet nginx");
   res.json({ message: "Backend: up and running mate." });
+});
+
+// receive data from frontend
+app.post("/data", async (req, res) => {
+  const data = await req.body;
+  last_saved_idx++;
+  console.log("received this data :", data);
+  res.json({ message: "Data saved successfully.", id: last_saved_idx });
 });
 
 app.listen(PORT, () => {
